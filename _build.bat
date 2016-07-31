b3712dcc03be38943741e73f660d2e1adf36d4b9
Kef Schecter <furrykef@gmail.com>
2016-07-31 09:21:34 -0500

    Tweak to game text; simplify build script

--- _build.bat
+++ _build.bat
@@ -1,16 +1,11 @@
 @echo off
-chars.py
-if errorlevel 1 goto error
-pwgen.py
-if errorlevel 1 goto error
-C:\MFS\tniasm\tniasm multibank.asm multibank.out
-if errorlevel 1 goto error
-copy tniasm.sym multibank.sym
-if errorlevel 1 goto error
-C:\MFS\tniasm\tniasm animalhack.asm animalhacke.rom
-if errorlevel 1 goto error
-"C:\Games\romhack\tools\text tools\Atlas-1.11\atlas" animalhacke.rom animalhack-script.txt
-if not errorlevel 1 goto end
+chars.py || goto error
+pwgen.py || goto error
+C:\MFS\tniasm\tniasm multibank.asm multibank.out || goto error
+copy tniasm.sym multibank.sym || goto error
+C:\MFS\tniasm\tniasm animalhack.asm animalhacke.rom || goto error
+"C:\Games\romhack\tools\text tools\Atlas-1.11\atlas" animalhacke.rom animalhack-script.txt || goto error
+goto end
 :error
 echo There were errors.
 :end
